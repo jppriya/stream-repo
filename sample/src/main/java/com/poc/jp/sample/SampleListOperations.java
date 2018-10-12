f946b3cb1ac2d4e860379833dcdcfe1996a7d0e4
Jayapriya R <jayapriya.r@tavant.com>
2018-10-12 16:28:40 +0530

    linked list

---
+++ sample/src/main/java/com/poc/jp/sample/SampleListOperations.java
@@ -0,0 +1,21 @@
+package com.jp.poc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleListOperations {
+
+	public static void main (String args[]) {
+		System.out.println("List Operations");
+		List<String> list = new ArrayList<>();
+		list.add("Jaya");
+		updateList(list);
+		//Size will be 2
+		System.out.println("List Size"+ list.size()); 
+	}
+
+	private static void updateList(List<String> list) {
+		list.add("Priya");
+	}
+	
+}
